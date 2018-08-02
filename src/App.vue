5bae31951fe9d95a229bf5463be9068b848027a5
Nick Hulea <naeluh@gmail.com>
2018-08-01 20:52:50 -0700

    working on local call to data via import in main.js

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app" class="Prose">
-    <div v-if="this.$store.getters.loading" id="load-component"><div class="parent"><div class="child">Loading ...</div></div></div>
-  	<Nav v-if="!this.$store.getters.loading" v-cloak></Nav>
-    <router-view v-if="!this.$store.getters.loading" :arr="this.$store.state.site" v-cloak></router-view>
+    <div v-if="$store.getters.loading" id="load-component"><div class="parent"><div class="child">Loading ...</div></div></div>
+  	<Nav v-if="!$store.getters.loading" v-cloak></Nav>
+    <router-view v-if="!$store.getters.loading" :arr="this.$store.state.site" v-cloak></router-view>
   </div>
 </template>
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
+import data from './assets/data/work.json'
 
 Vue.use(Vuex)
 
@@ -43,11 +44,13 @@ const mutations = {
 
 const actions = {
   LOAD_DATA (context, payload) {
-    let url = 'https://data.aiwinter.org/json?_format=json'
+    // let url = 'https://data.aiwinter.org/json?_format=json'
     context.commit('SET_LOADED', false)
+    console.log(data)
     axios
-      .get(url)
+      .get(data)
       .then(function (response) {
+        console.log(response)
         let data = response.data
 
         for (var i = data.length - 1; i >= 0; i--) {
